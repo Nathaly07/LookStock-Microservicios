a3e72df470c798d7d0b14af5b808d390e36dc7b5
Nathaly07 <nathaly.simba@epn.edu.ec>
2025-02-08 11:22:15 -0500

    feat: Add module in chat

--- microservices/chat-service/src/app.module.ts
+++ microservices/chat-service/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 1433,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      options: { encrypt: true, trustServerCertificate: false },
+    }),
+    AuthModule,
+    ChatModule,
+  ],
 })
 export class AppModule {}
